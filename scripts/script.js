30ff4130e8c34604d73b4dcaed9aeb78e2ab4642
Sujayweb3master <122873224+Sujayweb3master@users.noreply.github.com>
2023-10-19 10:49:26 +0530

    Add function, used to play one round of the game

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,53 @@
 
-    console.log("Display some text")
-
     function getComputerChoice() {
+        // define a variable of integer type
         let computerChoiceHolder = 0;
 
+        // assign random number between 1 - 3 to that variable
         computerChoiceHolder = Math.floor(Math.random() * 3) + 1;
 
+        // conditional statement to return one of the three choices
         switch(computerChoiceHolder) {
             case 1 : return "rock";
-                     break;
+                    //  break;
 
             case 2 : return "paper";
-                     break;
+                    //  break;
 
             case 3 : return "scissor";
-                     break;
+                    //  break;
 
             default : return null;
         }
     }
 
+    function playRound(playerSelection, computerSelection) {
+        
+        const defaultValue = "OOOps, it's a tie.";
+
+        switch(computerSelection) {
+            case "rock": if (playerSelection === "scissor") return "You Lose! Rock beats Scissor";
+                         else if (playerSelection === "paper") return "You Win! Paper beats Rock";
+                         else return defaultValue;
+
+            case "paper": if (playerSelection === "rock") return "You Lose! Paper beats Rock";
+                          else if (playerSelection === "scissor") return "You Win! Scissor beats Paper";
+                          else return defaultValue;
+
+            case "scissor": if (playerSelection === "paper") return "You Lose! Scissor beats Paper";
+                            else if (playerSelection === "rock") return "You Win! Rock beats Scissor";
+                            else return defaultValue;
+
+            default: return null;
+                         
+        }
+    }
+
+    let holder = prompt("Input your choice:");
+    const playerSelection = holder.toLowerCase();
+    const computerSelection = getComputerChoice();
+
+    console.log(computerSelection);
+    console.log(playerSelection);
+
+    console.log(playRound(playerSelection, computerSelection));
